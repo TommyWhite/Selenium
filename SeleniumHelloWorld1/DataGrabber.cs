f544d284e80c27ec0e1e499bcdd0b8f2ba0df0c1
Artem Moskalets <moskalets.artem@gmail.com>
2016-06-13 15:34:30 +0300

    Data driven logic from XML file implemented.

--- SeleniumHelloWorld1/DataGrabber.cs
+++ SeleniumHelloWorld1/DataGrabber.cs
@@ -2,7 +2,12 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
 using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading;
 
 namespace SeleniumHelloWorld
@@ -43,7 +48,7 @@ namespace SeleniumHelloWorld
             GC.SuppressFinalize(this);
         }
 
-        public static void GrabData(string taxNum)
+        public string GrabData(string taxNum)
         {
             driver.Navigate().GoToUrl(Globals.URL_GOV_US);
             driver.SwitchTo().Frame(driver.FindElement(By.ClassName(Globals.FRAME_CLASS_NAME)));
@@ -60,28 +65,86 @@ namespace SeleniumHelloWorld
             //var frame = WaitForElementToAppear(driver, 30, By.ClassName(FRAME_CLASS_NAME));
             var body = WaitForElementToAppear(driver, 30, By.TagName("body"));
 
-            string data = table.GetAttribute("innerHTML");
-            data = body.GetAttribute("innerHTML");
-            string codeBase = AppDomain.CurrentDomain.BaseDirectory;
-            string name = $"{Globals.TAX_NUMBER}.html";
-            SaveDataTo(codeBase, name, data);
+            string tableInner = table.GetAttribute("innerHTML");
+            string bodyInner = body.GetAttribute("innerHTML");
 
-            driver.Quit();
+            return tableInner;
         }
 
-        public static IWebElement WaitForElementToAppear(IWebDriver driver, int waitTime, By waitingElement)
+        public IWebElement WaitForElementToAppear(IWebDriver driver, int waitTime, By waitingElement)
         {
             IWebElement wait = new WebDriverWait(driver, TimeSpan.FromSeconds(waitTime)).Until(ExpectedConditions.ElementExists(waitingElement));
             return wait;
         }
 
-        public static void SaveDataTo(string baseFolder, string fileNameWithExt, string data)
+        public void SaveDataTo(string baseFolder, string fileNameWithExt, string data)
         {
             string output = Path.Combine(baseFolder, fileNameWithExt);
             using (TextWriter writer = File.CreateText(output))
             {
                 writer.WriteLine(data);
             }
+        }
+
+    }
+
+    public class ExcelDataProvider
+    {
+        public DataSet ReadExcelFile(string filePath)
+        {
+            DataSet ds = new DataSet();
+
+            string connectionString = GetConnectionString(filePath);
+
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                conn.Open();
+                OleDbCommand cmd = new OleDbCommand();
+                cmd.Connection = conn;
+
+                // Get all Sheets in Excel File
+                DataTable dtSheet = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+                string sheetName = "Sheet1$";
+                
+                cmd.CommandText = $"SELECT * FROM [{sheetName}]";
+
+                DataTable dt = new DataTable()
+                {
+                    TableName = sheetName
+                };
+
+                OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                da.Fill(dt);
+
+                ds.Tables.Add(dt);
+
+                conn.Close();
+            }
+            
+
+            return ds;
+        }
+
+        private string GetConnectionString(string fileName)
+        {
+            Dictionary<string, string> props = new Dictionary<string, string>();
+
+            props["Provider"] = "Microsoft.ACE.OLEDB.12.0;";
+            props["Extended Properties"] = "Excel 12.0 XML";
+            props["Data Source"] = fileName;
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (KeyValuePair<string, string> prop in props)
+            {
+                sb.Append(prop.Key);
+                sb.Append('=');
+                sb.Append(prop.Value);
+                sb.Append(';');
+            }
+
+            return sb.ToString();
         }
 
     }
--- SeleniumHelloWorld1/Program.cs
+++ SeleniumHelloWorld1/Program.cs
@@ -1,16 +1,16 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
 using System.IO;
-using System.Threading;
+using System.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace SeleniumHelloWorld
 {
     internal class Program
     {
-
         private static void Main(string[] args)
         {
             Run();
@@ -18,22 +18,49 @@ namespace SeleniumHelloWorld
 
         public static void Run()
         {
-            var settings = ConfigurationManager.GetSection(typeof(InputData).Name);
-            InputData inputData = settings as InputData;
+            // Initialize data via Configuration Section (need to be fixed)
+            //var settings = ConfigurationManager.GetSection(typeof(InputData).Name);
+            //InputData inputData = settings as InputData;
+            //string dataFile;
+            //if (inputData != null)
+            //{
+            //    dataFile = inputData.ExcelFile;
+            //}
+            //else
+            //{
+            //    throw new FileNotFoundException("Excel file with input data is not found!");
+            //}
 
-            if (inputData != null)
-            { 
-                string file = inputData.ExcelFile;
-            }
-            else
+            string dataFile = ConfigurationManager.AppSettings["excelWithTaxNumbers"];
+            if (string.IsNullOrEmpty(dataFile) && File.Exists(dataFile))
             {
                 throw new FileNotFoundException("Excel file with input data is not found!");
             }
 
+            var ddt = new ExcelDataProvider();
+            string fileName = "3.xlsx";
+            var filePath = $@"C:\Users\artemm\Desktop\Selenium\SeleniumHelloWorld1\bin\Debug\{fileName}";
+            var data = ddt.ReadExcelFile(filePath);
+
+            List<string> list = new List<string>();
+
+            foreach (DataRow row in data.Tables[0].Rows)
+            {
+                var number = row.ItemArray.Select(NO => NO.ToString()).ToList();
+                list.AddRange(number);
+            }
+
             DataGrabber grabber = new DataGrabber();
+
             try
             {
-
+                foreach (var item in list)
+                {
+                    string codeBase = AppDomain.CurrentDomain.BaseDirectory;
+                    string name = $"{Globals.TAX_NUMBER}.html";
+                    var grabbed = grabber.GrabData(item);
+                    grabber.SaveDataTo(codeBase, name, grabbed);
+                }
             }
             catch (Exception ex)
             {
@@ -45,4 +72,6 @@ namespace SeleniumHelloWorld
             }
         }
     }
+
+    
 }
